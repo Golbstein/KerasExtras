87845abbb401a8ff1bac64ffd2d2b57f3daecd9e
Jenia Golbstein <jenia@novellusdx.com>
2018-10-13 11:10:33 +0300

    Update keras_functions.py

--- keras_functions.py
+++ keras_functions.py
@@ -42,13 +42,11 @@ def sparse_accuracy_ignoring_last_label(y_true, y_pred):
 def Mean_IOU(y_true, y_pred):
     nb_classes = K.int_shape(y_pred)[-1]
     y_pred = K.reshape(y_pred, (-1, nb_classes))
-    y_true = tf.to_int32(K.reshape(y_true, (-1, 1))[:,0])
-    y_true = K.one_hot(y_true, nb_classes)
-    true_pixels = K.argmax(y_true, axis=-1)
+    true_pixels = tf.to_int32(K.reshape(y_true, (-1, 1))[:,0])
     pred_pixels = K.argmax(y_pred, axis=-1)
     iou = []
     flag = tf.convert_to_tensor(-1, dtype='float64')
-    for i in range(nb_classes-1): # exclude last label (void)
+    for i in range(1, nb_classes-1): # exclude first (background) and last label (void)
         true_labels = K.equal(true_pixels, i)
         pred_labels = K.equal(pred_pixels, i)
         inter = tf.to_int32(true_labels & pred_labels)
@@ -59,7 +57,6 @@ def Mean_IOU(y_true, y_pred):
     iou = tf.stack(iou)
     legal_labels = tf.greater(iou, flag)
     iou = tf.gather(iou, indices=tf.where(legal_labels))
-    print(iou)
     return K.mean(iou)
 
 def get_available_gpus():
