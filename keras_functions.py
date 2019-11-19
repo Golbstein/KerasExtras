0e97f3254d90010a8617182c1db7e122374426fc
Jenia Golbstein <jenia@novellusdx.com>
2019-11-19 12:44:24 +0200

    tf 2 support

--- keras_functions.py
+++ keras_functions.py
@@ -57,7 +57,7 @@ def sparse_Mean_IOU(y_true, y_pred):
     iou = tf.gather(iou, indices=tf.where(legal_labels))
     return K.mean(iou)
 
-def Mean_IOU(y_true, y_pred):
+def Mean_IOU_tensorflow_1(y_true, y_pred):
     nb_classes = K.int_shape(y_pred)[-1]
     iou = []
     true_pixels = K.argmax(y_true, axis=-1)
@@ -74,6 +74,26 @@ def Mean_IOU(y_true, y_pred):
     iou = tf.stack(iou)
     legal_labels = ~tf.debugging.is_nan(iou)
     iou = tf.gather(iou, indices=tf.where(legal_labels))
+    return K.mean(iou)
+
+
+def Mean_IOU_tensorflow_2(y_true, y_pred):
+    nb_classes = K.int_shape(y_pred)[-1]
+    iou = []
+    true_pixels = K.argmax(y_true, axis=-1)
+    pred_pixels = K.argmax(y_pred, axis=-1)
+    void_labels = K.equal(K.sum(y_true, axis=-1), 0)
+    for i in range(0, nb_classes): # exclude first label (background) and last label (void)
+        true_labels = K.equal(true_pixels, i) & ~void_labels
+        pred_labels = K.equal(pred_pixels, i) & ~void_labels
+        inter = tf.to_int32(true_labels & pred_labels)
+        union = tf.to_int32(true_labels | pred_labels)
+        legal_batches = K.sum(tf.to_int32(true_labels), axis=1)>0
+        ious = K.sum(inter, axis=1)/K.sum(union, axis=1)
+        iou.append(K.mean(ious[legal_batches]))
+    iou = tf.stack(iou)
+    legal_labels = ~tf.math.is_nan(iou)
+    iou = iou[legal_labels]
     return K.mean(iou)
 
 def get_available_gpus():
